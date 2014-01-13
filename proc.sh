8b6e8c6021cd51cae950d548bd7bb89ffd6f1cb0
dword1511 <zhangchi866@gmail.com>
2014-01-13 00:58:22 -0600

    proc.sh: random enhancement.

--- proc.sh
+++ proc.sh
@@ -4,15 +4,21 @@
 
 for image in src/*.jpg;
 do
-  cp $image tmp1.jpg
+  HASH=`md5sum -b "${image}" | cut -d " " -f 1`
+  if [ -e "output/${HASH}.jpg" ] && [ -e "output/${HASH}-thumb.jpg" ]
+  then
+    echo "Files for wallpaper ${HASH} (${image}) already exist, skipping (delete them to force update)."
+  else
+    cp "$image" tmp1.jpg
 
-  convert tmp1.jpg -quality 85 -resize 288x162 tmp2.jpg
-  jpegtran -optimize -progressive -copy none -outfile output/`md5sum -b $image | cut -d " " -f 1`-thumb.jpg tmp2.jpg
+    convert tmp1.jpg -resize 288x162 -quality 85 tmp2.jpg
+    jpegtran -optimize -progressive -copy none -outfile "output/${HASH}-thumb.jpg" tmp2.jpg
 
-  convert tmp1.jpg stamp.png -gravity northeast -geometry +2+2 -composite -quality 95 tmp2.jpg
-  jpegtran -optimize -progressive -copy none -outfile output/`md5sum -b $image | cut -d " " -f 1`.jpg tmp2.jpg
+    convert tmp1.jpg stamp.png -gravity northeast -geometry +2+2 -composite -quality 95 tmp2.jpg
+    jpegtran -optimize -progressive -copy none -outfile "output/${HASH}.jpg" tmp2.jpg
 
-  echo "$image done."
+    echo "${HASH} (${image}) done."
+  fi
 done
 
 rm -f tmp1.jpg tmp2.jpg
